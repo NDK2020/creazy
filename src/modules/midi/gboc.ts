eae37f08b6e8d52d7698513ca791b0bde43a8f86
ndk2020 <khoanguyen91@outlook.com>
2024-01-10 17:03:57 +0700

    fix (gboc): not exclude duplicate notes with first rnote

--- src/modules/midi/gboc.ts
+++ src/modules/midi/gboc.ts
@@ -130,6 +130,9 @@ export class GBOC {
     // @bh/@output
     //--------------
     let rnote_count = 0;
+    let first_rnote_exclude = final_notes.find(fn => fn.from_track.includes("relation"));
+    let first_rnote_exclude_ta = first_rnote_exclude?.time_appear.ticks;
+
     let output = final_notes.map((cur_note, i, self) => {
 
       //pid - position-id
@@ -196,12 +199,13 @@ export class GBOC {
           if (rnote_count == 1) {
             is_mc = "0";
             is_dis = "1";
+
+            console.log(`rnote count=1 ${i} is disabled`);
           }
 
           if (rnote_count > 1) {
             is_mc = "1";
             console.log(`cur note ${i}: has mood change`)
-
           }
 
           pid = "2"; // tile-for-mood-change is at middle
@@ -212,18 +216,37 @@ export class GBOC {
 
         if (!cur_note.from_track.includes("relation")) {
 
-          let has_overlap_is_mood_change_note = self.some(
-            (nn) =>
-              nn.time_appear.ticks == cur_note.time_appear.ticks &&
-              nn.from_track.includes("relation")
-          );
+          let has_overlap_is_mood_change_note =
+            self.some(
+              (nn) =>
+                nn.time_appear.ticks === cur_note.time_appear.ticks &&
+                nn.from_track.includes("relation")
+            );
 
-          if (has_overlap_is_mood_change_note) {
-            is_dis = "1";
+          let overlap_note = self.find(nn =>
+            nn.time_appear.ticks === cur_note.time_appear.ticks &&
+            nn.from_track.includes("relation")
+          )
+
+
+          if (first_rnote_exclude_ta === cur_note.time_appear.ticks) {
+            is_dis = "0";
+          } else {
+            if (has_overlap_is_mood_change_note) {
+              is_dis = "1";
+              console.log(`cur-note ${cur_note.order} is overlap-note ${overlap_note?.order}`);
+              console.log(`cur-note ta: ${cur_note.time_appear.ticks} ${cur_note.from_track}`)
+              console.log(`overlap_note ta: ${overlap_note?.time_appear.ticks} ${overlap_note?.from_track}`)
+              console.log("***********")
+            }
           }
 
+
         }
+
+
       }
+
 
       //----------------------------------------
       // detect gift item spawn on top tile
@@ -279,107 +302,122 @@ export class GBOC {
       this.total_notes = tmp.length;
 
       output = tmp.map((cur_note, i, self) => {
-      //pid - position-id
-      //|….1….3….5….|
-      //|…..2...4…..|
-      let pid = "none";
+        //pid - position-id
+        //|….1….3….5….|
+        //|…..2...4…..|
+        let pid = "none";
 
-      /* according to
-       * https://docs.google.com/spreadsheets/d/1o6sIngj9PwPIhuZE2mAUXRTvSfoSJzSiAY_xv0BJS7A/edit#gid=480870965
-       * this rule is currently incorrect
-      if (n.number == 84) {
-        pid = "0";
-      }
-  
-      if (n.number == 85) {
-        pid = "1";
-      }
-  
-      if (n.number == 86) {
-        pid = "2";
-      }
-  
-      if (n.number == 87) {
-        pid = "3";
-      }
-  
-      if (n.number == 88) {
-        pid = "4";
-      }
-      */
+        /* according to
+         * https://docs.google.com/spreadsheets/d/1o6sIngj9PwPIhuZE2mAUXRTvSfoSJzSiAY_xv0BJS7A/edit#gid=480870965
+         * this rule is currently incorrect
+        if (n.number == 84) {
+          pid = "0";
+        }
+    
+        if (n.number == 85) {
+          pid = "1";
+        }
+    
+        if (n.number == 86) {
+          pid = "2";
+        }
+    
+        if (n.number == 87) {
+          pid = "3";
+        }
+    
+        if (n.number == 88) {
+          pid = "4";
+        }
+        */
 
-      // now gboc rule is the same as tile-hop
-      if (cur_note.number == 96) {
-        pid = "0";
-      }
-
-      if (cur_note.number == 97) {
-        pid = "1";
-      }
-
-      if (cur_note.number == 98) {
-        pid = "2";
-      }
-
-      if (cur_note.number == 99) {
-        pid = "3";
-      }
-
-      if (cur_note.number == 100) {
-        pid = "4";
-      }
-
-      // moodchange
-      let is_mc = "0";
-      let is_dis = "0";
-      if (this.tracks.include_track_relation) {
-        if (cur_note.from_track.includes("relation")) {
-
-          console.log(`note relation: ${cur_note.number} - ${cur_note.time_appear.ticks}`)
-          console.log(` relation-note-count ${rnote_count}`);
-          console.log("********************");
-
-          rnote_count = rnote_count + 1;
-          if (rnote_count == 1) {
-            is_mc = "0";
-            is_dis = "1";
-          }
-
-          if (rnote_count > 1) {
-            is_mc = "1";
-            console.log(`cur note ${i}: has mood change`)
-
-          }
-
-          pid = "2"; // tile-for-mood-change is at middle
+        // now gboc rule is the same as tile-hop
+        if (cur_note.number == 96) {
+          pid = "0";
         }
 
-        // handle case where cur-note is normal note and
-        // have same time-appear with mood-change note
+        if (cur_note.number == 97) {
+          pid = "1";
+        }
+
+        if (cur_note.number == 98) {
+          pid = "2";
+        }
+
+        if (cur_note.number == 99) {
+          pid = "3";
+        }
+
+        if (cur_note.number == 100) {
+          pid = "4";
+        }
+
+        // moodchange
+        let is_mc = "0";
+        let is_dis = "0";
+        if (this.tracks.include_track_relation) {
+          if (cur_note.from_track.includes("relation")) {
+
+            console.log(`note relation: ${cur_note.number} - ${cur_note.time_appear.ticks}`)
+            console.log(` relation-note-count ${rnote_count}`);
+            console.log("********************");
+
+            rnote_count = rnote_count + 1;
+            if (rnote_count == 1) {
+              is_mc = "0";
+              is_dis = "1";
+            }
+
+            if (rnote_count > 1) {
+              is_mc = "1";
+              console.log(`cur note ${i}: has mood change`)
+
+            }
+
+            pid = "2"; // tile-for-mood-change is at middle
+          }
+
+          // handle case where cur-note is normal note and
+          // have same time-appear with mood-change note
 
         if (!cur_note.from_track.includes("relation")) {
 
-          let has_overlap_is_mood_change_note = self.some(
-            (nn) =>
-              nn.time_appear.ticks == cur_note.time_appear.ticks &&
-              nn.from_track.includes("relation")
-          );
+          let has_overlap_is_mood_change_note =
+            self.some(
+              (nn) =>
+                nn.time_appear.ticks === cur_note.time_appear.ticks &&
+                nn.from_track.includes("relation")
+            );
 
-          if (has_overlap_is_mood_change_note) {
-            is_dis = "1";
+          let overlap_note = self.find(nn =>
+            nn.time_appear.ticks === cur_note.time_appear.ticks &&
+            nn.from_track.includes("relation")
+          )
+
+
+          if (first_rnote_exclude_ta === cur_note.time_appear.ticks) {
+            is_dis = "0";
+          } else {
+            if (has_overlap_is_mood_change_note) {
+              is_dis = "1";
+              console.log(`cur-note ${cur_note.order} is overlap-note ${overlap_note?.order}`);
+              console.log(`cur-note ta: ${cur_note.time_appear.ticks} ${cur_note.from_track}`)
+              console.log(`overlap_note ta: ${overlap_note?.time_appear.ticks} ${overlap_note?.from_track}`)
+              console.log("***********")
+            }
           }
 
-        }
-      }
 
-      //----------------------------------------
-      // detect gift item spawn on top tile
-      let is_g = "0";
-      if (cur_note.number == 95) {
-        console.log(`is note has gift: id:${i}-${cur_note.time_appear.secs}`);
-        is_g = "1"
-        pid = "-1";
-      }
+        }
+
+        //----------------------------------------
+        // detect gift item spawn on top tile
+        let is_g = "0";
+        if (cur_note.number == 95) {
+          console.log(`is note has gift: id:${i}-${cur_note.time_appear.secs}`);
+          is_g = "1"
+          pid = "-1";
+        }
 
 
         let ta = self[i].time_appear.secs - cut_start_time;
@@ -388,29 +426,29 @@ export class GBOC {
             ? self[i].time_appear.secs
             : self[i].time_appear.secs - self[i - 1].time_appear.secs;
 
-      let id_str = `id:${i}`;
-      let n_str = `n:${cur_note.number}`;
-      let pid_str = `pid:${pid}`;
-      let ta_str = `ta:${ta}`;
-      let ts_str = `ts:${ts}`;
-      let d_str = `d:${cur_note.duration.secs}`;
-      let v_str = `v:${cur_note.velocity}`;
-      let mc_str = `mc:${is_mc}`;
-      let is_g_str = `ig:${is_g}`;
-      let is_dis_str = `idis:${is_dis}`;
+        let id_str = `id:${i}`;
+        let n_str = `n:${cur_note.number}`;
+        let pid_str = `pid:${pid}`;
+        let ta_str = `ta:${ta}`;
+        let ts_str = `ts:${ts}`;
+        let d_str = `d:${cur_note.duration.secs}`;
+        let v_str = `v:${cur_note.velocity}`;
+        let mc_str = `mc:${is_mc}`;
+        let is_g_str = `ig:${is_g}`;
+        let is_dis_str = `idis:${is_dis}`;
 
-      return [
-        id_str,
-        n_str,
-        pid_str,
-        ta_str,
-        ts_str,
-        d_str,
-        v_str,
-        mc_str,
-        is_g_str,
-        is_dis_str
-      ].join("-");
+        return [
+          id_str,
+          n_str,
+          pid_str,
+          ta_str,
+          ts_str,
+          d_str,
+          v_str,
+          mc_str,
+          is_g_str,
+          is_dis_str
+        ].join("-");
       });
     }
     return output.join(",");
